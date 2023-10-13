3bc39ebc44ee3d368b94f8d12eb95785fed55c8e
Tejas Itankar <tejasitankar94820@gmail.com>
2023-10-14 01:27:27 +0530

    made a dummy courses section

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -1,12 +1,120 @@
 import React from 'react'
 
 function courses() {
+
   return (
-    <div>
-        <h1>Search Bar</h1>
-        <h1>corousal</h1>
-        <h1>filtering</h1>
-        <h1>courses with scroll to load more</h1>
+    <div className='absolute top-16 md:top-24 left-0 h-screen md:h-5/6 w-screen flex flex-row overflow-hidden bg-blue-400'>
+        {/* left part */}
+        <div className="bg-blue-400 hidden md:block md:overflow-y-scroll w-1/5">
+            <h1> left part-1</h1>
+            <h1> left part-2</h1>
+            <h1> left part-3</h1>
+            <h1> left part-4</h1>
+            <h1> left part-5</h1>
+            <h1> left part-6</h1>
+            <h1> left part-7</h1>
+            <h1> left part-8</h1>
+            <h1> left part-9</h1>
+            <h1> left part-10</h1>
+            <h1> left part-11</h1>
+            <h1> left part-12</h1>
+            <h1> left part-13</h1>
+            <h1> left part-14</h1>
+            <h1> left part-15</h1>
+            <h1> left part-16</h1>
+            <h1> left part-17</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+            <h1> left part</h1>
+
+        </div>
+
+        {/* right part */}
+        <div className='bg-red-400 overflow-y-scroll w-full md:w-4/5'>
+            {/* search bar */}
+            <div className="items-center px-4 flex justify-center" >
+                <div className="relative mr-3">
+                    <div className="absolute top-3 left-3 items-center">
+                        <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                    </div>
+                    <input
+                        type="text"
+                        className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
+                        placeholder="Search Here..."
+                    />
+                </div>
+            </div>
+            <h1>corousal</h1>
+            <h1>filtering</h1>
+            <h1>courses with scroll to load more</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+            <h1> Right part</h1>
+
+
+        </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,58 @@
 export default function Home() {
   return (
-    <>
+    <div className="absolute top-24 left-0">
+      <div>Home-1</div>
       <div>Home</div>
-    </>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home-100</div>
+
+    </div>
   )
 }
