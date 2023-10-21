de120e921fb0f2ba7987723a3cd14ab12dbe2b25
Tejas Itankar <tejasitankar94820@gmail.com>
2023-10-22 02:32:33 +0530

    finally changed the favicon icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>DK Academy</title>
-        <link rel="shortcut icon" href="/logo.png" />
+        <link rel="shortcut icon" href="/favicon.ICO" />
       </Head>
       <Header />
       <Component {...pageProps} />
