712ae0836eec1b519fd21cf0eefd1d32a3d8a330
Tejas Itankar <tejasitankar94820@gmail.com>
2023-10-24 04:01:40 +0530

    completed courses page

--- data/contentList.ts
+++ data/contentList.ts
@@ -1,6 +1,7 @@
 interface ContentItem {
     id: string;
     title: string;
+    type: string;
     new: boolean;
     thumbnail: string;
     class: string;
@@ -8,6 +9,8 @@ interface ContentItem {
     old_price: string;
     discount: string;
     courseLink: string;
+    lecturesCount: string;
+    duration: string;
   }
   
 type ContentListType = ContentItem[];
@@ -15,146 +18,185 @@ type ContentListType = ContentItem[];
 export const contentList:ContentListType = [
     {
         id: "1",
-        title: "PYQ Quadratic Equations ddfdfd dfdfd dfdfdf",
+        title: "PYQ Quadratic Equations more content",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "2",
         title: "PYQ Quadratic Equations",
+        type: 'PDF',
         new: false,
-        thumbnail: '/thumb.png',
+        thumbnail: '/art.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "3",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "4",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "5",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "6",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "7",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "8",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "9",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "10",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "11",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "12",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
     {
         id: "13",
         title: "PYQ Quadratic Equations",
+        type: 'course',
         new: true,
         thumbnail: '/thumb.png',
         class: "JEE Aspirants",
         price: "2500",
         old_price: "5000",
         discount: "50",
-        courseLink: "https://www.dkacademy.com"
+        courseLink: "https://www.dkacademy.com",
+        lecturesCount: "90",
+        duration: "110",
     },
 
 ]
--- src/components/course/CourseRight/Content.jsx
+++ src/components/course/CourseRight/Content.jsx
@@ -14,24 +14,64 @@ function Content() {
         {
             contentList.map( (item) => (
                 <div key={item.id} className='border-gray-200 border-2 rounded shadow-lg shadow-indigo-500/50 p-2'>
+                    {/* top */}
                     <div className='flex h-12 w-full pt-1'>
-                      <h3 className='font-medium text-sm w-3/4'>{item.title}</h3>
+                      <h3 className='text-sm w-3/4 font-bold'>{item.title}</h3>
                       <p className='bg-yellow-300 w-8 text-center h-5 rounded-md p-0.5 text-xs ml-4 '>New</p>
                       <Image src='/watsapp.svg' height={10} width={10} alt="watsapp logo" 
                         className='h-4 w-4 ml-2 mt-0.5 cursor-pointer animate-pulse active:animate-ping' 
                         onClick={() => redirectWatsapp(item.title,item.courseLink)}
                       />
                     </div>
-                    <Image src={item.thumbnail} height={300} width={300} alt="thumbnail" 
-                      className='rounded-lg'
-                    />
+                    {/* thumbnail */}
+                    <div className='w-full h-36'>
+                      <Image src={item.thumbnail} height={300} width={300} alt="thumbnail" 
+                        className='rounded-lg w-full h-full'
+                      />
+                    </div>
+
+                    {/* Description */}
+                    <div className='flex flex-col'>
+                      <div className='flex h-6 w-full'>
+                        <Image src='/student.svg' width={3} height={3} alt='Student icon' 
+                          className='w-4 h-4 mt-2.5'
+                        />
+                        <p className='mt-2 ml-2 text-sm'>For {item.class}</p>
+                      </div>
+                      <div className='flex h-auto w-full'>
+                        <Image src='/clock.svg' width={3} height={3} alt='Student icon' 
+                          className='w-4 h-4 mt-2.5'
+                        />
+                        <p className='mt-2 ml-2 text-sm'>Duration {item.lecturesCount} lectures, {item.duration} hrs</p>
+                      </div>
+                    </div>
                     
-                    <p>{item.class}</p>
+                    {/* separator */}
                     <hr className='border border-purple-700'/>
-                    <div>
-                      <p>{item.price}</p>
-                      <p>{item.old_price}</p>
-                      <p> Discount of {item.discount}% applied </p>
+                    
+                    
+                    {/* bottom */}
+                    <div className='flex flex-col'>
+                      <div className='flex justify-between'>
+                        <div className='flex flex-col'>
+                          <div className='flex'> 
+                            <p className='text-base font-bold text-violet-900'>&#x20B9;{item.price}</p>
+                            <p className='text-sm line-through text-gray-600 ml-1 mt-0.5'>{item.old_price}</p>
+                          </div>
+                        <p className='text-xs'> &#40;For full {item.type}&#41; </p>
+                        </div>
+                        <div className='h-fit p-1 my-auto rounded-lg bg-green-300 flex items-center '>
+                          <Image src='/discount.svg' width={10} height={10} alt='Discount Icon' 
+                            className='w-4 h-4 mr-1 align-middle' 
+                          />
+                          <p className='text-xs align-middle'> Discount of {item.discount}% applied</p>
+                        </div>
+                      </div>
+                      {/* buttons */}
+                      <div className='grid grid-cols-2 gap-5 justify-between mt-2 font-semibold'>
+                        <button className='bg-violet-200 text-violet-800 hover:text-violet-200 hover:bg-violet-800 rounded-lg p-0.5 text-sm active:animate-ping'>Explore</button>
+                        <button className='bg-violet-700 text-white hover:text-violet-800 hover:bg-violet-200 rounded-lg text-sm active:animate-ping'>Buy Now</button>
+                      </div>
                     </div>
 
                 </div>
