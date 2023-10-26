9fa5bd74a40adbf478d235e6d812ece4e2d484e0
Tejas Itankar <tejasitankar94820@gmail.com>
2023-10-26 22:52:39 +0530

    vercel error fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react';
-import Header from '../components/header';
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
