1f0adc87c196766d58037290e3b457694e2f356d
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-03 16:05:34 +0530

    changed the port ip on developement and prod differently

--- src/axios/axiosClient.ts
+++ src/axios/axiosClient.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const IP_PORT = process.env.NODE_ENV === 'production' ? process.env.NODE_PROD_URL : 'http://localhost:5000'
 
 const axiosClient = axios.create({
-    baseURL: `http://65.0.1.227:5000/api/v1/`,
+    baseURL: `${IP_PORT}/api/v1/`,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
