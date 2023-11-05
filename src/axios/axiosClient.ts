3c92de74b95bcf6b2188cd71992ef331e9712cfa
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-05 20:53:30 +0530

    change the server api

--- src/axios/axiosClient.ts
+++ src/axios/axiosClient.ts
@@ -5,7 +5,7 @@ const dev = "http://localhost:5000/api/v1/";
 const prod = "https://api.dkacademy.co.in/api/v1/";
 
 const axiosClient = axios.create({
-    baseURL: dev,
+    baseURL: prod,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
