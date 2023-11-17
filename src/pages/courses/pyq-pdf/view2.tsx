f2ddbcf926ab0a3b379452a73d265b9952413d5e
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-17 16:19:37 +0530

    changed the dimensions of image and buttons in view2

--- src/pages/courses/pyq-pdf/view2.tsx
+++ src/pages/courses/pyq-pdf/view2.tsx
@@ -30,24 +30,24 @@ const View = () => {
   return (
     <HeaderLayout>
       <CourseLayout>
-        <div className={`scrollbar w-max h-full overflow-x-scroll md:overflow-x-hidden md:overflow-y-scroll md:w-5/6 p-2 `} onContextMenu={disableRightClick}>
-          <div className=' bg-sky-700 rounded-3xl p-4'>
+        <div className={`scrollbar w-max h-full overflow-x-scroll md:overflow-x-hidden md:overflow-y-scroll md:w-5/6 p-2  `} onContextMenu={disableRightClick}>
+          <div className=' bg-sky-700 rounded-3xl p-2  md:p-4 h-full md:h-max flex flex-col '>
             <Image
               src={`/cq-${currentPage}.png`}
               alt='circle question image'
               height={1000}
               width={1000}
-              className='print-hidden w-full rounded-3xl'
+              className='print-hidden w-full rounded-3xl h-5/6'
               onContextMenu={disableRightClick} // Attach the event handler
             />
-            <div className='mt-2 flex flex-col items-center '>
+            <div className='mt-2 flex flex-col items-center h-1/6 justify-center'>
               <p className='text-white'>Page {currentPage} of {totalPages}</p>
               <div className='flex justify-center gap-4 mt-2'>
                 <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={handlePrev}>Prev</button>
                 <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={handleNext}>Next</button>
               </div>
             </div>
-            <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={() => router.back()}>Back</button>
+            {/* <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={() => router.back()}>Back</button> */}
         </div>
           </div>
       </CourseLayout>
