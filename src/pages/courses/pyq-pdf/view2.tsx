0c1a3ecd3743708db9a43d2bf1628e58429dbd24
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-17 17:37:53 +0530

    added a back button and page navigator

--- src/pages/courses/pyq-pdf/view2.tsx
+++ src/pages/courses/pyq-pdf/view2.tsx
@@ -26,30 +26,41 @@ const View = () => {
     }
   };
 
+  const backHandler = () => {
+    router.back();
+  }
+
 
   return (
     <HeaderLayout>
       <CourseLayout>
-        <div className={`scrollbar w-max h-full overflow-x-scroll md:overflow-x-hidden md:overflow-y-scroll md:w-5/6 p-2  `} onContextMenu={disableRightClick}>
+        <div className={`scrollbar w-max h-full overflow-x-scroll md:overflow-x-hidden md:overflow-y-scroll md:w-5/6 p-2  relative`} onContextMenu={disableRightClick}>
+          {/* back button */}
+          <div className='absolute right-2 z-10'>
+            <button className='px-2 py-1 flex bg-white rounded w-20 hover:border-2 border-2 border-blue-600 hover:border-red-700 ' onClick={backHandler}>Back</button>
+          </div>
+          {/* page navigator */}
+          <div className='flex flex-col items-center absolute justify-center  w-full '>
+            <div className='rounded bg-sky-700 p-1 text-center'>
+              <p className='text-white'>Page {currentPage} of {totalPages}</p>
+              <div className='flex justify-center gap-4 mt-1'>
+                <button className=' bg-white rounded w-16 hover:border-2 hover:border-red-700' onClick={handlePrev}>Prev</button>
+                <button className=' bg-white rounded w-16 hover:border-2 hover:border-red-700' onClick={handleNext}>Next</button>
+              </div>
+            </div>
+          </div>
+          {/* pdf page */}
           <div className=' bg-sky-700 rounded-3xl p-2  md:p-4 h-full md:h-max flex flex-col '>
             <Image
               src={`/cq-${currentPage}.png`}
               alt='circle question image'
               height={1000}
               width={1000}
-              className='print-hidden w-full rounded-3xl h-5/6'
+              className='print-hidden h-full w-full rounded-3xl'
               onContextMenu={disableRightClick} // Attach the event handler
             />
-            <div className='mt-2 flex flex-col items-center h-1/6 justify-center'>
-              <p className='text-white'>Page {currentPage} of {totalPages}</p>
-              <div className='flex justify-center gap-4 mt-2'>
-                <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={handlePrev}>Prev</button>
-                <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={handleNext}>Next</button>
-              </div>
-            </div>
-            {/* <button className='px-2 py-1 bg-white rounded w-20 hover:border-2 hover:border-red-700' onClick={() => router.back()}>Back</button> */}
-        </div>
           </div>
+        </div>
       </CourseLayout>
     </HeaderLayout>
   );
