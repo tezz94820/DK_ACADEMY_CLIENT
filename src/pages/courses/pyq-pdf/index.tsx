7c44b082ce9fdb252ff123b3d4699c5dc96337d9
Tejas Itankar <tejasitankar94820@gmail.com>
2023-11-29 16:08:53 +0530

    adding the placeholder of gif as png files

--- src/pages/courses/pyq-pdf/index.tsx
+++ src/pages/courses/pyq-pdf/index.tsx
@@ -12,19 +12,22 @@ const PdfCourses = () => {
       id: "1",
       name: "Mathematics",
       image: "/mathematics.gif",
-      href: "/courses/pyq-pdf/mathematics"
+      href: "/courses/pyq-pdf/mathematics",
+      placeholder: "/mathematics.png",
     },
     {
       id: "2",
       name: "Physics",
       image: "/physics.gif",
-      href: "/courses/pyq-pdf/physics"
+      href: "/courses/pyq-pdf/physics",
+      placeholder: "/physics.png",
     },
     {
       id: "3",
       name: "Chemistry",
       image: "/chemistry.gif",
-      href: "/courses/pyq-pdf/chemistry"
+      href: "/courses/pyq-pdf/chemistry",
+      placeholder: "/chemistry.png",
     }
   ]
 
@@ -42,8 +45,8 @@ const PdfCourses = () => {
                 subjects.map( item => (
                   <Link key={item.id} href={item.href} className=' border-gray-200 border-2 rounded shadow-lg shadow-indigo-500/50 p-2 flex flex-col'>
                     <div className='h-44 w-full'>
-                      <Image src={item.image} height="200" width="200" alt={item.name} className='w-full h-full'/>
-                    </div>
+                      <Image src={item.image} height="200" width="200" alt={item.name} className='w-full h-full' placeholder="blur" blurDataURL={item.placeholder} />
+                    </div> 
                     <hr className='border border-purple-700 mt-2' />
                     <div className='mt-2 text-xl font-semibold'>{item.name}</div>
                   </Link>
