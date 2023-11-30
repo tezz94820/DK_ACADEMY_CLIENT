a0f5971828d8aabed572a65a83502986b83082fb
Tejas Itankar <tejasitankar94820@gmail.com>
2023-12-01 00:02:07 +0530

    made changes to video player when error

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -12,6 +12,8 @@ type VideoPlayerProps = {
 const VideoPlayer = ({videoLink}:VideoPlayerProps) => {
   console.log(videoLink);
 
+  const [error, setError] = useState<boolean>(false);
+
   const playerConfig = {
     file: {
       attributes: {
@@ -22,19 +24,34 @@ const VideoPlayer = ({videoLink}:VideoPlayerProps) => {
 
   const disableRightClick = (e: React.MouseEvent<HTMLImageElement>) => {
     e.preventDefault();
-};
+  };
+
+  const errorHandler = () => {
+    setError(true);
+    toast.error("Video Cannot be Fetched");
+
+  }
 
   return (
     <div onContextMenu={disableRightClick} className='flex justify-center items-center h-fit'>
       <div className='rounded-3xl p-2 bg-gradient-to-r from-indigo-600 via-purple-600 to-rose-500'>
         {/* <Image src="/logo.png" alt="logo" height={100} width={100} className='w-10 h-10 bg-red-600'/> */}
-        {/* <div className='bg-red-400'>Hello</div> */}
-        <ReactPlayer 
-          url={videoLink} 
-          controls
-          config={playerConfig} 
-          classname="mt-0"
-        />
+        {
+          error 
+          ? 
+          <div className='text-white font-bold'>No Video Found</div> 
+          :
+          <div>
+            <ReactPlayer 
+            url={videoLink} 
+            controls
+            config={playerConfig}
+            onError={errorHandler} 
+            classname="mt-0"
+            />
+            <div className='text-white font-bold text-center text-xl mt-2'> D.K SIR</div>
+          </div>
+        }
       </div>
       
     </div>
